5a6364edd2bbe16c17f05a5d8dec642f95b8a8e9
Rachid Boudjelida <rachidboudjelida@gmail.com>
2023-11-25 03:30:19 +0100

    use live test socket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:3001");
 
 function App() {
   const [message, setMessage] = useState("");
